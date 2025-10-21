6162ecbe390611e87ce92378bb520042c85deb50
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-10-21 08:40:42 +0200

    Add a function that creates the card element

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-import { genres } from "./modules/data.js";
+import { genres, podcasts } from "./modules/data.js";
 import { createOpt } from "./modules/createOption.js";
+import { createCard } from "./modules/createCard.js";
 const gDropdown = document.getElementById("genre-options");
+const podGrid = document.getElementById("pod-grid");
 
 genres.forEach((genre) => {
   createOpt(gDropdown, genre.title);
+});
+
+podcasts.forEach((podcast) => {
+  createCard(podGrid, podcast.image, podcast.title);
 });
---
+++ modules/createCard.js
@@ -0,0 +1,11 @@
+export function createCard(gridEl, cover, name) {
+  const card = document.createElement("div");
+  card.className = "card";
+  const image = document.createElement("img");
+  const title = document.createElement("p");
+  image.src = cover;
+  image.className = "cover";
+  title.textContent = name;
+  card.append(image, title);
+  gridEl.appendChild(card);
+}
