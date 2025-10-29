acaaa626c13d2122da28034a4657c27bc3a2f10f
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-10-29 09:48:53 +0200

    Add DOM elements for season, genres and last updated

--- modules/createCard.js
+++ modules/createCard.js
@@ -1,12 +1,33 @@
-export function createCard(gridEl, cover, name) {
+export function createCard({
+  cover,
+  name,
+  seasons,
+  genreTags = [],
+  lastUpdate,
+}) {
   const card = document.createElement("div");
   card.className = "card";
   const image = document.createElement("img");
   const title = document.createElement("p");
+  const nSeasons = document.createElement("p");
+  nSeasons.className = "seasons-text";
+  const tags = document.createElement("div");
+  tags.className = "genre-tags";
+  const dateUp = document.createElement("p");
+  dateUp.className = "updated-date";
+
+  genreTags.forEach((genre) => {
+    const tag = document.createElement("span");
+    tag.className = "tag";
+    tag.textContent = genre;
+    tags.append(tag);
+  });
 
   image.src = cover;
   image.className = "cover";
   title.textContent = name;
-  card.append(image, title);
-  gridEl.appendChild(card);
+  nSeasons.textContent = `${seasons} seasons`;
+  dateUp.textContent = lastUpdate;
+  card.append(image, title, nSeasons, tags, dateUp);
+  return card;
 }
