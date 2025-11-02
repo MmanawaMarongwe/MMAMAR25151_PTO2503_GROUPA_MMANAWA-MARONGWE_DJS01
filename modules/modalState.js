58c5316276b80c3966c227db3bcf72bc271787c4
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-11-02 23:08:51 +0200

    Fix duplication in the modalState module

--- modules/modalState.js
+++ modules/modalState.js
@@ -20,16 +20,3 @@ export function modalState(modalContainer) {
     }, // accepts the container; controls it internally
   };
 }
-export function modalState(modalContainer) {
-  return {
-    openModal(modalContent) {
-      modalContainer.innerHTML = "";
-      modalContainer.appendChild(modalContent);
-      modalContainer.style.display = "block";
-    },
-    closeModal() {
-      modalContainer.style.display = "none";
-      modalContainer.innerHTML = "";
-    }, // accepts the container; controls it internally
-  };
-}
